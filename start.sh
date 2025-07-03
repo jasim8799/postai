c68989b66b1163dc039c6b6fe1a94afc980635fc
MD JASIM <103302529+jasim8799@users.noreply.github.com>
2025-07-04 01:39:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-echo '✅ Installing Python dependencies...'
-pip install --upgrade pip
-pip install -r requirements.txt
-
-echo '✅ Installing Playwright browsers...'
-playwright install
-
 echo '✅ Starting background worker...'
 python main.py
