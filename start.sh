1e512676aa3862b572d2706c4a4e9668ccf55b88
MD JASIM <103302529+jasim8799@users.noreply.github.com>
2025-07-03 22:34:26 +0530

    Update 2 start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-echo '?? Installing Playwright browsers...'
+echo '✅ Installing Python dependencies...'
+pip install --upgrade pip
+pip install -r requirements.txt
+
+echo '✅ Installing Playwright browsers...'
 playwright install
 
-echo '?? Starting your Python app...'
-python main.py
+echo '✅ Starting your Python app...'
+uvicorn main:app --host 0.0.0.0 --port 10000
